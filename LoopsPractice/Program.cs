f5da8d7e00b926b575079810126e2e9bffc54a57
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-05 15:15:26 -0400

    reviewed concepts

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -10,6 +10,17 @@ namespace LoopsPractice
     {
         static void Main(string[] args)
         {
+            //When would you use a loop?
+            //Why would you use a loop?
+            //Nested loops
+            //Infinite loops
+            //Keywords break and continue
+
+            //For loop
+            //for(intializer; condition; updater0
+            //{
+            //  Do something
+            //}
         }
     }
 }
